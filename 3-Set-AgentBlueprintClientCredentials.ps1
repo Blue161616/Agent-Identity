d1f860adf8846056ccd5c0fb919cbc568bd978f7
Blue161616 <derk@blue16.nl>
2026-02-17 07:14:24 +0100

    Add files via upload

---
+++ 3-Set-AgentBlueprintClientCredentials.ps1
@@ -0,0 +1,22 @@
+Connect-MgGraph -Scopes "AgentIdentityBlueprint.AddRemoveCreds.All"
+
+$applicationId = "<agent-blueprint-app-id>"
+
+# Define the secret properties
+$displayName = "<Secret Name>"
+$endDate = (Get-Date).AddYears(1).ToString("o")  # 1 year from now, in ISO 8601 format
+
+# Construct the password credential
+$passwordCredential = @{
+    displayName = $displayName
+    endDateTime = $endDate
+}
+
+# Add the password (client secret)
+$response = Add-MgApplicationPassword -ApplicationId $applicationId -PasswordCredential $passwordCredential
+
+# Output the generated secret (only returned once!)
+Write-Host "Secret Text: $($response.secretText)"
+
+# Close the Graph
+Disconnect-MgGraph
